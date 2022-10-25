9e9c85eca072ae2a3635b078eb0019c479bf90de
Mark <mark28ten@gmail.com>
2022-10-24 17:07:06 -0700

    Adding email function

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,6 +10,7 @@ import {
   faJsSquare,
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders';
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -25,49 +26,52 @@ const About = () => {
   }, []);
 
   return (
-    <div className='container about-page'>
-      <div className='text-zone'>
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          Mark is a Code Fellows graduate in Full Stack Software Development in JavaScript. He is an Army veteran who is originally from Chicago. Right after the military, he received a dual degree in Electronic Music Production and Digital Audio engineering.
-        </p>
-        <p>
-          He then transitioned into the fitness field and became a personal trainer for a couple of years. After his time as a personal trainer, he decided to take a leap into the software development world. He decided to transition into software development because he likes to solve problems and work with teams.
-        </p>
-        <p>
-          He is highly adaptable and willing to learn whatever it takes to reach his goal. He chose to learn this skill so he can be competitive in the tech industry. His passion for tech is extremely vast; tech is all around and is growing day by day with no plans of stopping. He hopes that one day, his skills can be a part of that movement.
-        </p>
-      </div>
+    <>
+      <div className='container about-page'>
+        <div className='text-zone'>
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Mark is a Code Fellows graduate in Full Stack Software Development in JavaScript. He is an Army veteran who is originally from Chicago. Right after the military, he received a dual degree in Electronic Music Production and Digital Audio engineering.
+          </p>
+          <p>
+            He then transitioned into the fitness field and became a personal trainer for a couple of years. After his time as a personal trainer, he decided to take a leap into the software development world. He decided to transition into software development because he likes to solve problems and work with teams.
+          </p>
+          <p>
+            He is highly adaptable and willing to learn whatever it takes to reach his goal. He chose to learn this skill so he can be competitive in the tech industry. His passion for tech is extremely vast; tech is all around and is growing day by day with no plans of stopping. He hopes that one day, his skills can be a part of that movement.
+          </p>
+        </div>
 
-      <div className='stage-cube-cont'>
-        <div className='cubespinner'>
-          <div className='face1'>
-            <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
-          </div>
-          <div className='face2'>
-            <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
-          </div>
-          <div className='face3'>
-            <FontAwesomeIcon icon={faAngular} color="#28A4D9"/>
-          </div>
-          <div className='face4'>
-            <FontAwesomeIcon icon={faCss3} color="#5ED4F4"/>
-          </div>
-          <div className='face5'>
-            <FontAwesomeIcon icon={faReact} color="#EFD81D"/>
-          </div>
-          <div className='face6'>
-            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28"/>
+        <div className='stage-cube-cont'>
+          <div className='cubespinner'>
+            <div className='face1'>
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className='face2'>
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className='face3'>
+              <FontAwesomeIcon icon={faAngular} color="#28A4D9" />
+            </div>
+            <div className='face4'>
+              <FontAwesomeIcon icon={faCss3} color="#5ED4F4" />
+            </div>
+            <div className='face5'>
+              <FontAwesomeIcon icon={faReact} color="#EFD81D" />
+            </div>
+            <div className='face6'>
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 };
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Loader from 'react-loaders';
 // import Logo from './Logo';
 
 
@@ -11,20 +12,35 @@ const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
   // const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n'];
   const nameArray = ['a', 'r', 'k'];
+  // const jobArray = [
+  //   'w',
+  //   'e',
+  //   'b',
+  //   ' ',
+  //   'd',
+  //   'e',
+  //   'v',
+  //   'e',
+  //   'l',
+  //   'o',
+  //   'p',
+  //   'e',
+  //   'r',
+  //   '.',
+  // ]
   const jobArray = [
+    'S',
+    'o',
+    'f',
+    't',
     'w',
+    'a',
+    'r',
     'e',
-    'b',
     ' ',
-    'd',
+    'D',
     'e',
     'v',
-    'e',
-    'l',
-    'o',
-    'p',
-    'e',
-    'r',
     '.',
   ]
 
@@ -39,32 +55,35 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="containter home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Full-Stack / JavaScript / Veteran</h2>
-        <Link to="/contact" className="flat-button">CONTACT ME</Link>
+    <>
+      <div className="containter home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Full-Stack / JavaScript / Veteran</h2>
+          <Link to="/contact" className="flat-button">CONTACT ME</Link>
+        </div>
+        {/* <Logo /> */}
       </div>
-      {/* <Logo /> */}
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 };
 
